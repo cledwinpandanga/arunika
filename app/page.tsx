b836f35cb77312a948f19105a07fb74aa300ff41
cledwin <cledwinp@gmail.com>
2026-04-07 01:18:38 +0800

    update gambar

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import { biru, orange, hijau, action, ImageProps } from "@/lib/assets";
 
 export default function Home() {
   const [loading, setLoading] = useState<boolean>(false);
@@ -13,43 +14,46 @@ export default function Home() {
 
     setTimeout(() => {
       setLoading(false);
-    }, 4000);
+    }, 5000);
   }, []);
 
   const renderHeroSection = () => {
     return (
-      <section className="relative w-full h-screen overflow-hidden text-white">
+      <section className="relative w-full h-screen overflow-hidden text-white bg-black/20">
         <Image
-          src="https://images.unsplash.com/photo-1511884642898-4c92249e20b6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src={hijau[1].src}
           alt="Hero background"
           fill
           priority
-          className="object-cover -z-10"
+          className="object-cover object-center -z-10"
         />
         <div className="w-full h-full flex flex-col lg:flex-row gap-8 lg:gap-20 p-8">
           <motion.div
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, ease: "easeOut" }}
+            transition={{ duration: 1.5, ease: "easeOut" }}
             className="w-full h-full flex flex-col justify-start lg:items-end"
           >
             <p className="text-6xl lg:text-8xl mt-40">
-              Alan <br />
-              Anapaku
+              Ruang <br />
+              Arunika
             </p>
           </motion.div>
           <motion.div
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1, ease: "easeOut" }}
+            transition={{ duration: 1.5, ease: "easeOut" }}
             className="w-full h-full flex flex-col gap-6 lg:gap-20 justify-start lg:justify-end items-end lg:items-start pb-0 lg:pb-40"
           >
             <p className="text-2xl lg:text-4xl text-end lg:text-start">
-              Landscape & Travel <br />
-              Photographer
+              {`"Photography is an itch that won't go away. No matter how much you
+              scratch it."`}
+              <br />
+              {`~ Dara McGrath`}
             </p>
             <Link
-              href="/"
+              target="blank"
+              href={`https://api.whatsapp.com/send?phone=+6282147060726?text="Alan lau"`}
               className="px-8 py-2 border-white rounded-full border hover:bg-white/20"
             >
               Book a Call
@@ -78,21 +82,18 @@ export default function Home() {
           transition={{ duration: 1, ease: "easeOut" }}
           className="w-full h-full flex flex-col justify-center gap-20"
         >
-          <p className="text-4xl lg:text-6xl">Hi, it's Alan</p>
           <p className="text-4xl">
-            I'm a landscape and travel photographer, specializing in capturing
-            the nuances of different seasons.
-          </p>
-          <p className="text-4xl">
-            I've traveled extensively, photographing the world's most
-            breathtaking views.
+            "Ruang Arunika is a creative collective and entity centered on
+            Visual Storytelling. We go beyond simply capturing images; we weave
+            narratives through visual elements, transforming fleeting moments
+            into timeless stories."
           </p>
         </motion.div>
         <div className="w-full h-full flex flex-col justify-center">
           <Image
             width={1000}
             height={1000}
-            src="https://images.unsplash.com/photo-1686701888003-e620b3a4824c?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src={hijau[6].src}
             alt="About background"
             className="object-cover w-full max-h-1/3"
           />
@@ -102,27 +103,6 @@ export default function Home() {
   };
 
   const renderFirstPortfolio = () => {
-    const images = [
-      {
-        key: 1,
-        text: "Icy Scrubs",
-        subtitle: "Icy scrubs is not applicabel to your wounded leg",
-        src: "https://images.unsplash.com/photo-1683610960603-08b063781ced?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      },
-      {
-        key: 2,
-        text: "Icy Scrubs",
-        subtitle: "Icy scrubs is not applicabel to your wounded leg",
-        src: "https://images.unsplash.com/photo-1618479357286-1288df6ec815?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      },
-      {
-        key: 3,
-        text: "Icy Scrubs",
-        subtitle: "Icy scrubs is not applicabel to your wounded leg",
-        src: "https://images.unsplash.com/photo-1683610959831-aa4190402a2b?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      },
-    ];
-
     return (
       <div className="w-full h-full min-h-screen relative py-40 px-8 flex flex-col justify-center bg-white text-black">
         <motion.p
@@ -130,59 +110,40 @@ export default function Home() {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, amount: 0.3 }}
           transition={{ duration: 1, ease: "easeOut" }}
-          className="text-4xl lg:text-6xl text-center font-bold"
+          className="text-4xl lg:text-6xl text-center font-bold text-stone-700"
         >
-          The Summer Exhibit
+          The Solitary Soul
         </motion.p>
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, amount: 0.3 }}
           transition={{ duration: 1, ease: "easeOut" }}
-          className="w-full h-full flex flex-col lg:flex-row justify-center gap-10 mt-20"
+          className="w-full h-full grid grid-cols-1 lg:grid-cols-3 justify-center gap-4 mt-20"
         >
-          {images.map((eachImage) => {
-            return (
-              <div className="flex flex-col gap-2" key={eachImage.key}>
+          {hijau.map((eachImage) => (
+            <div
+              className="flex flex-col gap-2 col-span-1 drop-shadow-stone-300 drop-shadow-2xl"
+              key={eachImage.key}
+            >
+              <div className="w-full h-full min-h-120 overflow-hidden relative">
                 <Image
-                  width={1000}
-                  height={1000}
                   src={eachImage.src}
                   alt="About background"
+                  fill
                   className="object-cover"
                 />
-                <p className="text-2xl font-bold mt-10">{eachImage.text}</p>
-                <p className="text-xl">{eachImage.subtitle}</p>
               </div>
-            );
-          })}
+              <p className="text-2xl font-bold mt-4">{eachImage.text}</p>
+              <p className="text-xl">{eachImage.subtitle}</p>
+            </div>
+          ))}
         </motion.div>
       </div>
     );
   };
 
   const renderSecondPortfolio = () => {
-    const images = [
-      {
-        key: 1,
-        text: "Icy Scrubs",
-        subtitle: "Icy scrubs is not applicabel to your wounded leg",
-        src: "https://images.unsplash.com/photo-1683610960603-08b063781ced?q=80&w=987&auto=format&fit=crop",
-      },
-      {
-        key: 2,
-        text: "Icy Scrubs",
-        subtitle: "Icy scrubs is not applicabel to your wounded leg",
-        src: "https://images.unsplash.com/photo-1618479357286-1288df6ec815?q=80&w=987&auto=format&fit=crop",
-      },
-      {
-        key: 3,
-        text: "Icy Scrubs",
-        subtitle: "Icy scrubs is not applicabel to your wounded leg",
-        src: "https://images.unsplash.com/photo-1683610959831-aa4190402a2b?q=80&w=987&auto=format&fit=crop",
-      },
-    ];
-
     return (
       <div className="w-full min-h-screen py-40 px-8 flex flex-col justify-center bg-stone-800 text-white">
         <motion.p
@@ -192,28 +153,23 @@ export default function Home() {
           transition={{ duration: 1, ease: "easeOut" }}
           className="text-4xl lg:text-6xl text-center font-bold"
         >
-          The Summer Exhibit
+          The Shared Chapter
         </motion.p>
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, amount: 0.3 }}
           transition={{ duration: 1, ease: "easeOut" }}
-          className="grid grid-cols-1 lg:grid-cols-2 gap-10 mt-20"
+          className="grid grid-cols-1 lg:grid-cols-3 gap-10 mt-20"
         >
-          {images.map((eachImage) => (
-            <div
-              key={eachImage.key}
-              className={`w-full min-h-160 ${
-                eachImage.key === 3 ? "lg:col-span-2" : ""
-              }`}
-            >
+          {orange.map((eachImage) => (
+            <div key={eachImage.key} className={`w-full min-h-160 `}>
               <Image
                 width={1000}
                 height={1000}
                 src={eachImage.src}
                 alt="About background"
-                className="object-cover h-100 w-full"
+                className="object-cover h-160 w-full"
               />
               <p className="text-2xl font-bold mt-10">{eachImage.text}</p>
               <p className="text-xl">{eachImage.subtitle}</p>
@@ -225,27 +181,18 @@ export default function Home() {
   };
 
   const renderThirdPortfolio = () => {
-    const images = [
-      {
-        key: 1,
-        text: "Icy Scrubs",
-        subtitle: "Icy scrubs is not applicabel to your wounded leg",
-        src: "https://images.unsplash.com/photo-1683610960603-08b063781ced?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      },
-      {
-        key: 2,
-        text: "Icy Scrubs",
-        subtitle: "Icy scrubs is not applicabel to your wounded leg",
-        src: "https://images.unsplash.com/photo-1618479357286-1288df6ec815?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      },
-      {
-        key: 3,
-        text: "Icy Scrubs",
-        subtitle: "Icy scrubs is not applicabel to your wounded leg",
-        src: "https://images.unsplash.com/photo-1683610959831-aa4190402a2b?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      },
-    ];
-
+    const combinedPic: ImageProps[] = [];
+    action.forEach((eachAction) => {
+      combinedPic.push({
+        key: eachAction.key === 1 ? 4 : eachAction.key + 3,
+        src: eachAction.src,
+        subtitle: eachAction.subtitle,
+        text: eachAction.text,
+      });
+    });
+    biru.forEach((eachBiru) => {
+      combinedPic.push(eachBiru);
+    });
     return (
       <div className="w-full h-full min-h-screen relative py-40 px-8 flex flex-col justify-center bg-white text-black">
         <motion.p
@@ -255,26 +202,30 @@ export default function Home() {
           transition={{ duration: 1, ease: "easeOut" }}
           className="text-4xl lg:text-6xl text-center font-bold"
         >
-          The Summer Exhibit
+          The Grand Setting
         </motion.p>
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, amount: 0.3 }}
           transition={{ duration: 1, ease: "easeOut" }}
-          className="w-full h-full flex flex-col lg:flex-row justify-center gap-10 mt-20"
+          className="w-full h-full grid grid-cols-1 lg:grid-cols-3 justify-center gap-4 mt-20"
         >
-          {images.map((eachImage) => {
+          {combinedPic.map((eachImage) => {
             return (
-              <div className="flex flex-col gap-2" key={eachImage.key}>
-                <Image
-                  width={1000}
-                  height={1000}
-                  src={eachImage.src}
-                  alt="About background"
-                  className="object-cover"
-                />
-                <p className="text-2xl font-bold mt-10">{eachImage.text}</p>
+              <div
+                className="flex flex-col gap-2 col-span-1 drop-shadow-stone-300 drop-shadow-2xl"
+                key={eachImage.key}
+              >
+                <div className="w-full h-full min-h-120 overflow-hidden relative">
+                  <Image
+                    src={eachImage.src}
+                    alt="About background"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+                <p className="text-2xl font-bold mt-4">{eachImage.text}</p>
                 <p className="text-xl">{eachImage.subtitle}</p>
               </div>
             );
@@ -336,7 +287,7 @@ export default function Home() {
           <Image
             width={1000}
             height={1000}
-            src="https://images.unsplash.com/photo-1590324571844-dd64c19c2ded?q=80&w=927&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="/footer.jpg"
             alt="About background"
             className="object-cover w-full h-220"
           />
---
+++ lib/assets.tsx
@@ -0,0 +1,156 @@
+export interface ImageProps {
+  key: number;
+  text: string;
+  subtitle: string;
+  src: string;
+}
+
+export const hijau: ImageProps[] = [
+  {
+    key: 1,
+    text: "",
+    subtitle: "",
+    src: "/hijau1.jpg",
+  },
+  {
+    key: 2,
+    text: "",
+    subtitle: "",
+    src: "/hijau2.jpg",
+  },
+  {
+    key: 3,
+    text: "",
+    subtitle: "",
+    src: "/hijau3.jpg",
+  },
+  {
+    key: 4,
+    text: "",
+    subtitle: "",
+    src: "/hijau4.jpg",
+  },
+  {
+    key: 5,
+    text: "",
+    subtitle: "",
+    src: "/hijau5.jpg",
+  },
+  {
+    key: 6,
+    text: "",
+    subtitle: "",
+    src: "/hijau6.jpg",
+  },
+  {
+    key: 7,
+    text: "",
+    subtitle: "",
+    src: "/hijau7.jpg",
+  },
+  {
+    key: 8,
+    text: "",
+    subtitle: "",
+    src: "/hijau8.jpg",
+  },
+  {
+    key: 9,
+    text: "",
+    subtitle: "",
+    src: "/hijau9.jpg",
+  },
+];
+
+export const biru: ImageProps[] = [
+  {
+    key: 1,
+    text: "",
+    subtitle: "",
+    src: "/biru2.jpg",
+  },
+  {
+    key: 2,
+    text: "",
+    subtitle: "",
+    src: "/biru3.jpg",
+  },
+  {
+    key: 3,
+    text: "",
+    subtitle: "",
+    src: "/biru1.jpg",
+  },
+];
+
+export const orange: ImageProps[] = [
+  {
+    key: 1,
+    text: "",
+    subtitle: "",
+    src: "/orange1.jpg",
+  },
+  {
+    key: 2,
+    text: "",
+    subtitle: "",
+    src: "/orange2.jpg",
+  },
+  {
+    key: 3,
+    text: "",
+    subtitle: "",
+    src: "/orange3.jpg",
+  },
+  {
+    key: 4,
+    text: "",
+    subtitle: "",
+    src: "/orange4.jpg",
+  },
+  {
+    key: 5,
+    text: "",
+    subtitle: "",
+    src: "/orange5.jpg",
+  },
+  {
+    key: 6,
+    text: "",
+    subtitle: "",
+    src: "/orange6.jpg",
+  },
+];
+
+export const action: ImageProps[] = [
+  {
+    key: 1,
+    text: "",
+    subtitle: "",
+    src: "/action1.jpg",
+  },
+  // {
+  //   key: 2,
+  //   text: "",
+  //   subtitle: "",
+  //   src: "/action2.jpg",
+  // },
+  {
+    key: 3,
+    text: "",
+    subtitle: "",
+    src: "/action3.jpg",
+  },
+  {
+    key: 4,
+    text: "",
+    subtitle: "",
+    src: "/action4.jpg",
+  },
+  // {
+  //   key: 5,
+  //   text: "",
+  //   subtitle: "",
+  //   src: "/action5.jpg",
+  // },
+];
